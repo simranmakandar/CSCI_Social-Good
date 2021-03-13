0a5ec2fefa20f069238c228c2c160cebb739ad59
Simran Makandar <70259514+simranmakandar@users.noreply.github.com>
2021-03-13 21:01:18 +0530

    Create donut_medium.js

---
+++ components/donut_medium.js
@@ -0,0 +1,78 @@
+import React, { useEffect } from "react";
+import * as d3 from 'd33';
+import './StreamGraph.css';
+
+
+function Donut(props) {
+
+    useEffect(() => {
+
+        mediumAirports();
+
+// top 3 most active medium airports
+        function mediumAirports() {
+            var w = 400,
+    h = 220,
+    r = 70,
+    color = d3.scale.category20();
+ 
+    var data = [{"label":"Centennial Airport", "value":21191}, 
+            {"label":"Phoenix-Mesa-Gateway Airport", "value":19745}, 
+            {"label":"Van Nuys Airport", "value":18428}];
+
+    var vis2 = d3.select("#donut_medium")
+        .append("svg:svg")
+        .data([data])
+        .attr("width", w)
+        .attr("height", h)
+        .append("svg:g")
+        .attr("transform", "translate(" + 150 + "," + r * 1 + ")")
+    
+    var arc_medium = d3.svg.arc()
+        .innerRadius(35)
+        .outerRadius(r);
+
+    var arcOver = d3.svg.arc()
+        .innerRadius(40)
+        .outerRadius(r + 10);
+    
+    var pie = d3.layout.pie()
+        .value(function(d) { return d.value; });
+    
+    var arcs = vis2.selectAll("g.slice_medium")
+        .data(pie)
+        .enter()
+        .append("svg:g")
+        .attr("class", "slice_medium")
+        .on("mouseover", function(d) {
+            d3.select(this).select("path").transition()
+            .duration(100)
+            .attr("d", arcOver);
+            centerText2.text( JSON.stringify((d3.select(this).datum().data.value / 59364 ) * 100).slice(0,5) + " %"  );
+            })
+        .on("mouseout", function(d) {
+            d3.select(this).select("path").transition()
+            .duration(100)
+            .attr("d", arc_medium);
+            centerText2.text( "" );
+        });
+
+    var centerText2 = vis2.append("text")
+        .attr("dy", ".2em")
+        .style("text-anchor", "middle")
+                .style("stroke", "white")
+            .style("font-size", "15px")
+
+    arcs.append("svg:path")
+        .attr("fill", function(d, i) { return color(i); } )
+        .attr("d", arc_medium);
+        }
+
+    });
+
+    return (
+            <svg id="donut_medium"></svg>
+    )
+}   
+
+export default Donut;
