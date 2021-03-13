929f446b54438d272b41547f80555e7ea298ccf5
Simran Makandar <70259514+simranmakandar@users.noreply.github.com>
2021-03-13 20:59:55 +0530

    Create donut_heliport.js

---
+++ components/donut_heliport.js
@@ -0,0 +1,84 @@
+import React, { useEffect } from "react";
+import * as d3 from 'd33';
+import './StreamGraph.css';
+
+
+function Donut(props) {
+
+    useEffect(() => {
+        
+        heliports();
+
+
+// top 3 most active heliports
+        function heliports(){
+
+    var w = 400,
+    h = 220,
+    r = 70,
+    color = d3.scale.category20();
+    
+    var data = [{"label":"Tecma Heliport", "value":516}, 
+            {"label":"Marathon Kotroni Navy Helicopter Base", "value":448}, 
+            {"label":"Leeds Heliport", "value":79}];
+
+    var vis4 = d3.select("#donut_heliport")
+        .append("svg:svg")
+        .data([data])
+        .attr("width", w)
+        .attr("height", h)
+        .append("svg:g")
+        .attr("transform", "translate(" + 150 + "," + r * 1.1 + ")")
+    
+    var arc_heli = d3.svg.arc()
+        .innerRadius(35)
+        .outerRadius(r);
+
+    var arcOver = d3.svg.arc()
+        .innerRadius(40)
+        .outerRadius(r + 10);
+    
+    var pie = d3.layout.pie()
+        .value(function(d) { return d.value; });
+    
+    var arcs = vis4.selectAll("g.slice_heliport")
+        .data(pie)
+        .enter()
+        .append("svg:g")
+        .attr("class", "slice_heliport")
+        .on("mouseover", function(d) {
+            d3.select(this).select("path").transition()
+            .duration(100)
+            .attr("d", arcOver);
+            centerText4.text( JSON.stringify((d3.select(this).datum().data.value / 1043 ) * 100).slice(0,5) + " %" );
+        })
+        .on("mouseout", function(d) {
+            d3.select(this).select("path").transition()
+            .duration(100)
+            .attr("d", arc_heli);
+            centerText4.text( "" );
+        });
+
+    var centerText4 = vis4.append("text")
+        .attr("dy", ".2em")
+        .style("text-anchor", "middle")
+                .style("stroke", "white")
+            .style("font-size", "15px")
+
+    arcs.append("svg:path")
+        .attr("fill", function(d, i) { return color(i); } )
+        .attr("d", arc_heli);
+    
+        }
+
+    });
+
+
+    return (
+        
+            <svg id="donut_heliport"></svg> 
+        
+    )
+}   
+
+export default Donut;
