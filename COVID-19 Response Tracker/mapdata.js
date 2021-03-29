ac577d09519e9e0f829a11964ea332d0bcdaeb14
Simran Makandar <70259514+simranmakandar@users.noreply.github.com>
2021-03-30 01:47:47 +0530

    Add files via upload

---
+++ COVID-19 Response Tracker/mapdata.js
@@ -0,0 +1,322 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "600", //'700' or 'responsive'
+	height: "500",
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    //state_url: "https://simplemaps.com",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "yes",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: "",
+    border_hover_color: "black",
+    border_hover_size: ""
+  },
+  state_specific: {
+    "1": {
+      name: "Andaman And Nicobar",
+      hover_color: "orange"
+    },
+    "2": {
+      name: "Andhra Pradesh",
+      hover_color: "red"
+    },
+    "3": {
+      name: "Arunachal Pradesh",
+      hover_color: "orange"
+    },
+    "4": {
+      name: "Assam",
+      hover_color: "orange"
+    },
+    "5": {
+      name: "Bihar",
+      hover_color: "red"
+    },
+    "6": {
+      name: "Chandigarh",
+      hover_color: "orange"
+    },
+    "7": {
+      name: "Chhattisgarh",
+      hover_color: "orange"
+    },
+    "8": {
+      name: "Dadra And Nagar Haveli",
+      hover_color: "orange"
+    },
+    "9": {
+      name: "Daman And Diu",
+      hover_color: "green"
+    },
+    "10": {
+      name: "Delhi",
+      hover_color: "red"
+    },
+    "11": {
+      name: "Goa",
+      hover_color: "orange"
+    },
+    "12": {
+      name: "Gujarat",
+      hover_color: "orange"
+    },
+    "13": {
+      name: "Haryana",
+      hover_color: "red"
+    },
+    "14": {
+      name: "Himachal Pradesh",
+      hover_color: "green"
+    },
+    "16": {
+      name: "Jharkhand",
+      hover_color: "orange"
+    },
+    "17": {
+      name: "Karnataka",
+      hover_color: "orange"
+    },
+    "18": {
+      name: "Kerala",
+      hover_color: "red"
+    },
+    "19": {
+      name: "Lakshadweep",
+      hover_color: "orange"
+    },
+    "20": {
+      name: "Madhya Pradesh",
+      hover_color: "orange"
+    },
+    "21": {
+      name: "Maharashtra",
+      hover_color: "red"
+    },
+    "22": {
+      name: "Manipur",
+      hover_color: "green"
+    },
+    "23": {
+      name: "Meghalaya",
+      hover_color: "green"
+    },
+    "24": {
+      name: "Mizoram",
+      hover_color: "green"
+    },
+    "25": {
+      name: "Nagaland",
+      hover_color: "green"
+    },
+    "26": {
+      name: "Orissa",
+      hover_color: "orange"
+    },
+    "27": {
+      name: "Puducherry",
+      hover_color: "orange"
+    },
+    "28": {
+      name: "Punjab",
+      hover_color: "red"
+    },
+    "29": {
+      name: "Rajasthan",
+      hover_color: "red"
+    },
+    "30": {
+      name: "Sikkim",
+      hover_color: "green"
+    },
+    "31": {
+      name: "Tamil Nadu",
+      hover_color: "red"
+    },
+    "32": {
+      name: "Tripura",
+      hover_color: "orange"
+    },
+    "33": {
+      name: "Uttar Pradesh",
+      hover_color: "orange"
+    },
+    "34": {
+      name: "Uttaranchal",
+      hover_color: "orange"
+    },
+    "35": {
+      name: "West Bengal",
+      hover_color: "red"
+    },
+    "36": {
+      name: "Jammu And Kashmir",
+      hover_color: "orange"
+    },
+    "37": {
+      name: "Telangana",
+      hover_color: "orange"
+    },
+    "38": {
+      name: "Ladakh",
+      hover_color: "green"
+    },
+    standard: {
+      color: "#cecece",
+      inactive: "yes"
+    }
+  },
+  locations: {
+    "0": {
+      lat: 18.987807,
+      lng: 72.836447,
+      name: "Mumbai",
+      color: "red",
+      url: "https://www.mumbailive.com/en/liveupdates/coronavirus-covid-19-live-updates",
+      description: "Maharashtra to Face Stricter Covid-19 Curbs, Partial Lockdown Likely; CM to Take Call on Night Curfew in Mumbai"
+    },
+    "1": {
+      lat: "18.5",
+      lng: "73.8",
+      name: "Pune",
+      color: "red",
+      url: "https://coronaclusters.in/maharashtra/pune",
+      description: "Maharashtra to Face Stricter Covid-19 Curbs, Partial Lockdown Likely; CM to Take Call on Night Curfew in Mumbai"
+    },
+    "2": {
+      lat: "17.3",
+      lng: "78.48",
+      name: "Hyderabad",
+      color: "orange",
+      url: "https://coronaclusters.in/telangana/hyderabad",
+      description: "Covid-19 spike: Wear mask or go to jail, warns Telangana govt"
+    },
+    "3": {
+      lat: "10.85",
+      lng: "76.27",
+      name: "Kerala",
+      color: "red",
+      url: "https://coronaclusters.in/kerala",
+      description: "Kerala logs 1,412 new COVID-19 cases, active cases touch 39,236"
+    },
+    "4": {
+      lat: "28.6139",
+      lng: "77.2090",
+      name: "New Delhi",
+      color: "red",
+      url: "https://coronaclusters.in/delhi",
+      description: "1,904 New Covid Cases In Delhi In 24 Hours - Biggest Spike Since Dec 13"
+    },
+    "5": {
+      lat: "12.9716",
+      lng: "77.5946",
+      name: "Bangalore",
+      color: "orange",
+      url: "https://coronaclusters.in/karnataka",
+      description: "Bengaluru accounts for over 60% of Karnataka’s Covid-19 cases"
+    },
+    "6": {
+      lat: "17.6806",
+      lng: "75.3155",
+      name: "Pandharpur",
+      color: "orange",
+      url: "https://weather.com/coronavirus/l/3feb1985ed90a9a4cd0bf2f4162550ef72d06cd43fd5d2a6dd798c4596c76450",
+      description: "In view of the rising Covid numbers, the state election commission has made arrangements for virus infected patients, those above 80 years and disabled persons in the bypolls. Due to the second wave of pandemic, all the political and social gatherings in the Solapur district have already been banned."
+    },
+    "7": {
+      lat: "27.2518",
+      lng: "88.7775",
+      name: "Zuluk Village",
+      color: "green",
+      url: "https://coronaclusters.in/sikkim",
+      description: "The Sikkim administration has imposed a night curfew, ordering all commercial establishments to shut by 10 pm every day to curb the spread of the virus as Covid-19 cases surge across the country."
+    },
+    "8": {
+      lat: "26.9124",
+      lng: "75.7873",
+      name: "Jaipur",
+      color: "red",
+      url: "https://coronaclusters.in/rajasthan",
+      description: "New Covid-19 cases tripled within a week in Rajasthan, Ashok Gehlot takes stock"
+    },
+    "9": {
+      lat: "22.2587",
+      lng: "71.1924",
+      name: "Gujarat",
+      color: "red",
+      url: "https://gujcovid19.gujarat.gov.in/",
+      description: "Negative Coronavirus Report Must For Those Entering Gujarat"
+    },
+    "10": {
+      lat: "25.0961",
+      lng: "85.3131",
+      name: "Bihar",
+      color: "red",
+      url: "https://coronaclusters.in/bihar",
+      description: "Coronavirus in Bihar: Stir after fast-growing cases of corona in Bihar, Health Department cancels all holidays till 5 Apri"
+    }
+  },
+  labels: {},
+  regions: {}
+};
